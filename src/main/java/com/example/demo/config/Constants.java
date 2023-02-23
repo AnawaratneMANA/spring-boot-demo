a28070db8fc3bd913195da4d4d9cf3276ec0a053
AnawaratneMANA <it19086408@my.sliit.lk>
2023-02-23 22:10:34 +0530

    COMMIT: Add const classes

---
+++ src/main/java/com/example/demo/config/Constants.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+public final class Constants {
+    public static final String APPLICATION_JSON_VALUE = "application/json";
+    public static final class AgentProperties {
+        public static final String AGENT = "agent";
+    }
+
+    public static final class HttpCodesMessages {
+
+        public static final int HTTP_200_CODE = 200;
+        public static final java.lang.String HTTP_200_MESSAGE = "OK";
+
+        public static final int HTTP_400_CODE = 400;
+        public static final java.lang.String HTTP_400_MESSAGE = "Bad Request";
+
+        public static final int HTTP_500_CODE = 500;
+        public static final java.lang.String HTTP_500_MESSAGE = "Internal Server Errors";
+    }
+}
