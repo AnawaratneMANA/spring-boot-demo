2b1b789281d74a2bcdad36b7a399fa0c40a02654
AnawaratneMANA <it19086408@my.sliit.lk>
2023-02-24 13:28:40 +0530

    COMMIT: Constants Configuration updated with dev tokens

--- src/main/java/com/example/demo/config/Constants.java
+++ src/main/java/com/example/demo/config/Constants.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 public final class Constants {
     public static final String APPLICATION_JSON_VALUE = "application/json";
+    public static final String API = "/api";
+    public static final String VERSION = "/v2";
     public static final class AgentProperties {
-        public static final String AGENT = "agent";
+        public static final String AGENT = "/agent";
+
     }
 
     public static final class HttpCodesMessages {
@@ -16,5 +19,9 @@ public final class Constants {
 
         public static final int HTTP_500_CODE = 500;
         public static final java.lang.String HTTP_500_MESSAGE = "Internal Server Errors";
+    }
+
+    public static final class devProperties {
+        public static final String temp_auth_token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ3cXotTTE0bkNuejM3aHNJTDZycW9qd1JpQlZyV1dXTFhZRlRnZXhPdkZJIn0.eyJleHAiOjE2NzcyNTMxMTMsImlhdCI6MTY3NzIxNzExMywianRpIjoiM2MzZmNiNDYtMGQwMi00YmE3LWI5ZjktYWE3ZmI2YjQ3MmRkIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgxL3JlYWxtcy9tYXN0ZXIiLCJhdWQiOlsibWFzdGVyLXJlYWxtIiwiYWNjb3VudCJdLCJzdWIiOiI2OGUzZThjNC1kZTQzLTQ4YTEtOWM0Zi0yMTEwNmVhNDZhNjMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhZG1pbi1yZXN0LWNsaWVudCIsImFjciI6IjEiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1tYXN0ZXIiLCJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsibWFzdGVyLXJlYWxtIjp7InJvbGVzIjpbInZpZXctdXNlcnMiLCJxdWVyeS1ncm91cHMiLCJxdWVyeS11c2VycyJdfSwiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjbGllbnRIb3N0IjoiMTcyLjE3LjAuMSIsImNsaWVudElkIjoiYWRtaW4tcmVzdC1jbGllbnQiLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJzZXJ2aWNlLWFjY291bnQtYWRtaW4tcmVzdC1jbGllbnQiLCJjbGllbnRBZGRyZXNzIjoiMTcyLjE3LjAuMSJ9.fKm6v7js_RTTX9Tz5sTXjazAkabvPWLfMR_Hom-pJPHy65mNhac6WeW-7xnBwZUwqh-62MKUN2-Zeeq0ftE0KQ1ZeQfKinJYulpnJdO9dCGEKX_91LYMRoB1o_oocFs1XBGuyfcbya20kJSHXNCt2R5BRfg3LBNQSH7-wmJvyRgctAEY9hgl2_NQT2feA2bgIchAP9KVrKVl6eYXrV41BJDmrwbSQF1o9h4qOPwktoOobvNbzU5p9Rd0IlExdnzdWJj0NRFTTfOGJjEhpxq4nAmX0ezJWQzRhr-l0x1IKlBzYgrh-YsLHWenuZtiW9JuunEvCo3fSTQYxhfOZah5zg";
     }
 }
